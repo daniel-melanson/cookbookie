c4f9019656b87a77fb886e636de8629044347de5
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-19 10:53:58 -0400

    feat: units

--- src/makeFakeData.ts
+++ src/makeFakeData.ts
@@ -1,10 +1,42 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import { UnitSystem } from "@prisma/client";
 import { prisma } from "./server/db";
 
+async function makeUnits() {
+  const units = [
+    ["tsp", "teaspoon", "US"],
+    ["tbsp", "tablespoon", "US"],
+    ["fl oz", "fluid ounce", "US"],
+    ["c", "cup", "US"],
+    ["pt", "pint", "US"],
+    ["qt", "quart", "US"],
+    ["gal", "gallon", "US"],
+    ["oz", "ounce", "US"],
+    ["lb", "pound", "US"],
+    ["L", "litre", "METRIC"],
+    ["mL", "millilitre", "METRIC"],
+    ["g", "gram", "METRIC"],
+    ["kg", "kilograms", "METRIC"],
+  ] satisfies [string, string, UnitSystem][];
+
+  for (const [abbreviation, name, system] of units) {
+    const count = await prisma.unit.count({ where: { abbreviation } });
+    if (count !== 0) continue;
+
+    await prisma.unit.create({
+      data: {
+        abbreviation,
+        name,
+        system,
+      },
+    });
+  }
+}
+
 async function main() {
-  console.log(await prisma.unit.count());
+  await makeUnits();
 }
 
 void main();
