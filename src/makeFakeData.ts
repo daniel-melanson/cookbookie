372a8e7297c5fcc680b3459ed2eedb64388b4a07
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-19 13:56:01 -0400

    feat: tag kinds + tags

--- src/makeFakeData.ts
+++ src/makeFakeData.ts
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import { faker } from "@faker-js/faker";
 import type { UnitSystem } from "@prisma/client";
 import { prisma } from "./server/db";
+
+function uniqueWords(count: number): string[] {
+  const set = new Set<string>();
+  for (let i = 0; i < count; i++) {
+    set.add(faker.lorem.words({ min: 1, max: 3 }));
+  }
+
+  return [...set];
+}
 
 async function makeUnits() {
   const units = [
@@ -35,8 +45,27 @@ async function makeUnits() {
   }
 }
 
+async function makeTags() {
+  const tagCount = await prisma.tag.count();
+  if (tagCount !== 0) return;
+
+  const tagKinds = uniqueWords(faker.number.int({ min: 5, max: 15 }));
+  await prisma.tagKind.createMany({
+    data: tagKinds.map((name) => ({ name })),
+  });
+
+  const tags = uniqueWords(faker.number.int({ min: 30, max: 70 }));
+  await prisma.tag.createMany({
+    data: tags.map((name) => ({
+      kindName: faker.helpers.arrayElement(tagKinds),
+      name,
+    })),
+  });
+}
+
 async function main() {
   await makeUnits();
+  await makeTags();
 }
 
 void main();
