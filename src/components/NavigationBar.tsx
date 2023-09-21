03ec56c27386ceca5c96d8f2adcd58a8b288ac76
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-21 17:49:29 -0400

    feat: conditionally render search and icons

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -11,46 +11,62 @@ import {
 import IconLink from "./IconLink";
 import Link from "next/link";
 
-export default function NavigationBar() {
+interface NavigationBarProps {
+  includeSearch?: boolean;
+  // TODO change to a user or session object or something
+  user?: boolean;
+}
+
+export default function NavigationBar(props: NavigationBarProps) {
   return (
     <header className="align-center mb-9 flex h-16 items-center justify-between border-b border-light-gray px-4 py-2">
       <h1 className="text-3xl">
         <Link href="/">CookBookie</Link>
       </h1>
-      <form
-        className="align-center flex w-96 items-center space-x-2 rounded-lg bg-white px-4 py-2"
-        action="/recipes/search"
-        method="GET"
-      >
-        <RiSearchLine
-          className="align-self-middle text-lg
+      {props.includeSearch && (
+        <form
+          className="align-center flex w-96 items-center space-x-2 rounded-lg bg-white px-4 py-2"
+          action="/recipes/search"
+          method="GET"
+        >
+          <RiSearchLine
+            className="align-self-middle text-lg
           text-neutral-400"
-        />
-        <input
-          className="bg-transparent placeholder-neutral-400 focus:outline-none"
-          required
-          type="search"
-          name="q"
-          placeholder="Search..."
-        />
-      </form>
+          />
+          <input
+            className="bg-transparent placeholder-neutral-400 focus:outline-none"
+            required
+            type="search"
+            name="q"
+            placeholder="Search..."
+          />
+        </form>
+      )}
       <div className="flex items-center justify-end space-x-4 text-3xl">
-        <IconLink
-          href="/user/bookmarks"
-          line={<RiBookmarkLine />}
-          fill={<RiBookmarkFill />}
-        />
-        <IconLink
-          href="/user/calendar"
-          line={<RiCalendarLine />}
-          fill={<RiCalendarFill />}
-        />
-        <IconLink
-          href="/user/pantry"
-          line={<RiFridgeLine />}
-          fill={<RiFridgeFill />}
-        />
-        <div className="h-8 w-8 rounded-full bg-blue" />
+        {props.user ? (
+          <>
+            <IconLink
+              href="/user/bookmarks"
+              line={<RiBookmarkLine />}
+              fill={<RiBookmarkFill />}
+            />
+            <IconLink
+              href="/user/calendar"
+              line={<RiCalendarLine />}
+              fill={<RiCalendarFill />}
+            />
+            <IconLink
+              href="/user/pantry"
+              line={<RiFridgeLine />}
+              fill={<RiFridgeFill />}
+            />
+            <div className="h-8 w-8 rounded-full bg-blue" />
+          </>
+        ) : (
+          <Link className="text-lg" href="/signin">
+            Sign In
+          </Link>
+        )}
       </div>
     </header>
   );
