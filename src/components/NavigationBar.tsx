e657a540cec8b063e1a77bb5c7658476056cd66b
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-21 15:59:53 -0400

    feat: header

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { TbFridge, TbBookmark, TbCalendar, TbSearch } from "react-icons/tb";
 
 export default function NavigationBar() {
-  return <header className="mb-9">NavigationBar</header>;
+  return (
+    <header className="align-center mb-9 flex h-16 items-center justify-between border-b border-light-gray px-4 py-2">
+      <h1 className="text-3xl">CookBookie</h1>
+      <form className="align-center flex w-96 items-center space-x-2 rounded-lg bg-white px-4 py-2">
+        <TbSearch
+          className="align-self-middle
+          text-lg"
+        />
+        <input
+          className="bg-transparent focus:outline-none"
+          type="search"
+          placeholder="Search..."
+        />
+      </form>
+      <div className="flex justify-end space-x-4 text-4xl">
+        <TbBookmark />
+        <TbCalendar />
+        <TbFridge />
+        <div className="h-9 w-9 rounded-full bg-blue" />
+      </div>
+    </header>
+  );
 }
