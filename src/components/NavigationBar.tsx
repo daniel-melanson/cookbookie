b8714894d2050b8890fe0f51ea097d2ba0abbf9c
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-21 17:33:28 -0400

    feat: links and form actions

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -9,19 +9,28 @@ import {
   RiSearchLine,
 } from "react-icons/ri";
 import IconLink from "./IconLink";
+import Link from "next/link";
 
 export default function NavigationBar() {
   return (
     <header className="align-center mb-9 flex h-16 items-center justify-between border-b border-light-gray px-4 py-2">
-      <h1 className="text-3xl">CookBookie</h1>
-      <form className="align-center flex w-96 items-center space-x-2 rounded-lg bg-white px-4 py-2">
+      <h1 className="text-3xl">
+        <Link href="/">CookBookie</Link>
+      </h1>
+      <form
+        className="align-center flex w-96 items-center space-x-2 rounded-lg bg-white px-4 py-2"
+        action="/recipes/search"
+        method="GET"
+      >
         <RiSearchLine
           className="align-self-middle text-lg
           text-neutral-400"
         />
         <input
           className="bg-transparent placeholder-neutral-400 focus:outline-none"
+          required
           type="search"
+          name="q"
           placeholder="Search..."
         />
       </form>
