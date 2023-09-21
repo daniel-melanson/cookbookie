787580638a5077150b0bd0b862cc14a6b999413f
RyeCai <70669000+RyeCai@users.noreply.github.com>
2023-09-20 23:01:00 -0400

    Configured theming for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,16 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'blue': '#1878b9',
+      'light-green': '#6eb87c',
+      'gold': '#ecab32',
+      'orange': '#eb4a36',
+      'white': '#ffffff',
+      'black': '#0c0808',
+      'gray': '#b2a9a9',
+      'light-gray': '#e7dada',
+    },
     extend: {},
   },
   plugins: [],
