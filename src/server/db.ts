1e64536acb848ef23f3a424d449a2c89aa76270d
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-27 11:41:37 -0400

    feat: extend prisma for checks

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,6 +1,16 @@
+/* eslint-disable */
+// @ts-nocheck
+
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env.mjs";
+import {
+  UserCreateInput,
+  IngredientCreateInput,
+  RecipeCreateInput,
+  TagKindCreateInput,
+  TagCreateInput,
+} from "~/utils/validators";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -12,5 +22,39 @@ export const prisma =
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
+
+const validate = (Model) => ({
+  create({ args, query }) {
+    args.data = Model.parse(args.data);
+
+    return query(args);
+  },
+  update({ args, query }) {
+    args.data = Model.partial().parse(args.data);
+
+    return query(args);
+  },
+  updateMany({ args, query }) {
+    args.data = Model.partial().parse(args.data);
+
+    return query(args);
+  },
+  upsert({ args, query }) {
+    args.create = Model.parse(args.create);
+    args.update = Model.partial().parse(args.update);
+
+    return query(args);
+  },
+});
+
+prisma.$extends({
+  query: {
+    user: validate(UserCreateInput),
+    ingredient: validate(IngredientCreateInput),
+    recipe: validate(RecipeCreateInput),
+    tagKind: validate(TagKindCreateInput),
+    tag: validate(TagCreateInput),
+  },
+});
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
