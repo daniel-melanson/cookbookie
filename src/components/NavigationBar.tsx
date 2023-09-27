3c928d34835fa9ff36fdd8748bb43cd6d2471473
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-27 12:10:51 -0400

    feat: componentize searchbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -6,10 +6,10 @@ import {
   RiCalendarFill,
   RiBookmarkLine,
   RiBookmarkFill,
-  RiSearchLine,
 } from "react-icons/ri";
 import IconLink from "./IconLink";
 import Link from "next/link";
+import SearchBar from "./SearchBar";
 
 interface NavigationBarProps {
   includeSearch?: boolean;
@@ -23,25 +23,7 @@ export default function NavigationBar(props: NavigationBarProps) {
       <h1 className="text-3xl">
         <Link href="/">CookBookie</Link>
       </h1>
-      {props.includeSearch && (
-        <form
-          className="align-center flex w-96 items-center space-x-2 rounded-lg bg-white px-4 py-2"
-          action="/recipes/search"
-          method="GET"
-        >
-          <RiSearchLine
-            className="align-self-middle text-lg
-          text-neutral-400"
-          />
-          <input
-            className="bg-transparent placeholder-neutral-400 focus:outline-none"
-            required
-            type="search"
-            name="q"
-            placeholder="Search..."
-          />
-        </form>
-      )}
+      {props.includeSearch && <SearchBar />}
       <div className="flex items-center justify-end space-x-4 text-3xl">
         {props.user ? (
           <>
