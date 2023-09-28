9a358c2613dc63ba899398a3dfbac5f5dd837126
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-28 14:20:40 -0400

    fix: use radix-ui primitives over themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { Theme } from "@radix-ui/themes";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import "@radix-ui/themes/styles.css";
-import { Theme } from '@radix-ui/themes';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -15,9 +12,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Theme>
-        <Component {...pageProps} />
-      </Theme>
+      <Component {...pageProps} />
     </SessionProvider>
   );
 };
