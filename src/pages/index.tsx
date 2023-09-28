8a09199e7d4a13d32976bed59344dd955e177137
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-28 15:54:19 -0400

    fix: homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,36 +11,11 @@ export default function Home() {
       <Head>
         <title>CookBookie</title>
       </Head>
+      <Script src="https://accounts.google.com/gsi/client" async defer></Script>
       <NavigationBar user={true} includeSearch={true} />
       <HomeMain />
-      <Script src="https://accounts.google.com/gsi/client" async defer></Script>
-      <NavigationBar />
-      <main className=""></main>
       <Footer />
     </>
   );
 }
 
-// function AuthShowcase() {
-//   const { data: sessionData } = useSession();
-//
-//   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-//     undefined, // no input
-//     { enabled: sessionData?.user !== undefined },
-//   );
-//
-//   return (
-//     <div className="flex flex-col items-center justify-center gap-4">
-//       <p className="text-center text-2xl text-white">
-//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-//         {secretMessage && <span> - {secretMessage}</span>}
-//       </p>
-//       <button
-//         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-//         onClick={sessionData ? () => void signOut() : () => void signIn()}
-//       >
-//         {sessionData ? "Sign out" : "Sign in"}
-//       </button>
-//     </div>
-//   );
-// }
