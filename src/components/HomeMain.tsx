49831a61491814cf1d7ce53334376ee053446110
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-28 23:27:32 -0400

    feat: componentized search button

--- src/components/HomeMain.tsx
+++ src/components/HomeMain.tsx
@@ -3,8 +3,8 @@ import SearchBar, { SearchBarStyle } from "./SearchBar";
 
 export default function HomeMain() {
   return (
-    <main className="mx-10">
-      <div className="homeImageBanner flex h-64  flex-col items-center justify-center rounded-bl-lg rounded-br-lg">
+    <main className="lg:mx-10">
+      <div className="homeImageBanner flex h-[400px]  flex-col items-center justify-center rounded-bl-lg rounded-br-lg">
         <SearchBar style={SearchBarStyle.Home} />
       </div>
     </main>
