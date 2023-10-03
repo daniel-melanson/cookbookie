45330053eb0bf25bcd0ff9e461dbb91e06f8ff2b
RyeCai <70669000+RyeCai@users.noreply.github.com>
2023-10-03 16:42:22 -0400

    added warning icon to error messages

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 // import { /* useSession, */ signIn /*, signOut */ } from "next-auth/react";
 import * as Form from "@radix-ui/react-form";
 import Link from "next/link";
+import { AiOutlineWarning } from "react-icons/ai";
 import { ContinueWithGoogle } from "~/components/form/ContinueWith";
 import FormInput from "~/components/form/FormInput";
-import FormButton from "~/components/form/FormButton";
 
 function SignInForm() {
   return (
-    <Form.Root className="w-full">
+    <Form.Root className="flex w-full flex-col items-center">
       <Form.Field className="mb-3 w-full" name="email">
         <div className="flex items-baseline justify-between">
           <Form.Label className="leading-9">Email</Form.Label>
-          <Form.Message className="text-sm font-light" match="valueMissing">
-            Please enter your email
+          <Form.Message
+            className="flex items-center text-sm font-light text-red-600"
+            match="valueMissing"
+          >
+            <AiOutlineWarning />
+            <div className="ml-1">Please enter your email</div>
           </Form.Message>
-          <Form.Message className="text-sm font-light" match="typeMismatch">
-            Please enter a valid email
+          <Form.Message
+            className="flex items-center text-sm font-light text-red-600"
+            match="typeMismatch"
+          >
+            <AiOutlineWarning />
+            <div className="ml-1">Please enter a valid email</div>
           </Form.Message>
         </div>
         <FormInput type="email" />
@@ -24,8 +32,12 @@ function SignInForm() {
       <Form.Field className="mb-3 w-full" name="password">
         <div className="flex items-baseline justify-between">
           <Form.Label className="leading-9">Password</Form.Label>
-          <Form.Message className="text-sm font-light" match="valueMissing">
-            Please enter your password
+          <Form.Message
+            className="flex items-center text-sm font-light text-red-600"
+            match="valueMissing"
+          >
+            <AiOutlineWarning />
+            <div className="ml-1">Please enter your password</div>
           </Form.Message>
         </div>
         <FormInput type="password" />
@@ -38,23 +50,28 @@ function SignInForm() {
           Sign In
         </button>
       </Form.Submit>
+      <div className="py-4">
+        <Link href="">Forgot Password?</Link>
+      </div>
       <ContinueWithGoogle />
     </Form.Root>
   );
 }
 
 export default function SignIn() {
+  const [currentForm, setForm] = useState("Sign In");
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-br from-amber-400 to-red-600">
       <h2 className="mb-10 text-center font-cursive text-5xl font-black text-white">
         CookBookie
       </h2>
       <div className="flex h-max w-96 flex-col items-center rounded-md  bg-white px-4 py-4 md:px-8 md:py-8">
+        <h2 className="font-semibold">Sign In</h2>
         <SignInForm />
         <div className="mt-8 text-xs font-light">
           <p>
             Don&apos;t have an account?{" "}
-            <Link className=" underline" href="/signup">
+            <Link className="font-bold hover:underline " href="/signup">
               Sign up here
             </Link>
             .
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -19,6 +19,32 @@ export default function SignUp() {
         </div>
         <FormInput type="email" />
       </Form.Field>
+      <Form.Field className="mb-3 w-full" name="password">
+        <div className="flex items-baseline justify-between">
+          <Form.Label className="leading-9">Password</Form.Label>
+          <Form.Message className="text-sm font-light" match="valueMissing">
+            Please enter a password
+          </Form.Message>
+        </div>
+        <FormInput type="password" />
+      </Form.Field>
+      <Form.Field className="mb-3 w-full" name="password">
+        <div className="flex items-baseline justify-between">
+          <Form.Label className="leading-9">Password</Form.Label>
+          <Form.Message className="text-sm font-light" match="valueMissing">
+            Please re-enter your password
+          </Form.Message>
+        </div>
+        <FormInput type="password" />
+      </Form.Field>
+      <Form.Submit className="mt-3 w-full" asChild>
+        <button
+          className="h-[35px] w-full items-center justify-center rounded bg-neutral-950 text-white hover:shadow-sm hover:shadow-neutral-400"
+          type="submit"
+        >
+          Sign Up
+        </button>
+      </Form.Submit>
     </Form.Root>
   );
 }
