11c43da62d58004288fa2e402ccf31a99674ce5b
RyeCai <70669000+RyeCai@users.noreply.github.com>
2023-10-04 21:08:23 -0400

    Componetized form messages

---
+++ src/components/form/FormMessage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import * as Form from "@radix-ui/react-form";
+import { AiOutlineWarning } from "react-icons/ai";
+
+export interface MessageProps {
+  // Types found in source code
+  match: Form.ValidityMatcher | Form.CustomMatcher;
+  msg: string;
+}
+
+export function FormInputError({ match, msg }: MessageProps) {
+  return (
+    <Form.Message
+      className="flex items-center text-sm font-light text-red-600"
+      match={match}
+    >
+      <AiOutlineWarning />
+      <div className="ml-1">{msg}</div>
+    </Form.Message>
+  );
+}
