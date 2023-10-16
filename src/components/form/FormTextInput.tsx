f7a18868757de9860719417dfadba72c21ad8b2e
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-10-16 12:37:57 -0400

    fix: style

--- src/components/form/FormTextInput.tsx
+++ src/components/form/FormTextInput.tsx
@@ -13,7 +13,7 @@ export default function FormTextInput({ type, name }: InputProps) {
       name={name}
       type={type}
       placeholder="" // keep placeholder empty to use label as placeholder
-      className="focus:border-blue peer h-[48px] w-full rounded border-[1px] border-neutral-200 px-2 text-sm leading-none placeholder-transparent transition-colors hover:border-neutral-300 focus:outline-none data-[invalid]:border-red-500"
+      className="focus:border-blue peer h-[48px] w-full rounded border-[1px] border-neutral-200 px-2 text-sm leading-none placeholder-transparent transition-colors hover:border-neutral-300 focus:border-neutral-500 focus:outline-none data-[invalid]:border-red-500"
       required
     />
   );
