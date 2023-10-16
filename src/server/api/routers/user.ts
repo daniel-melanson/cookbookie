b519af993a72368571943459c6a8470a262d04e8
RyeCai <70669000+RyeCai@users.noreply.github.com>
2023-10-16 18:18:12 -0400

    setup userRouter

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+  getUser: publicProcedure
+    .input(z.object({email: z.string(), password: z.string()}))
+    .query(async ({ ctx, input }) => {
+    return await ctx.prisma.user.findUnique({where: {
+      email: input.email,
+      password: input.password
+    }});
+  }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
