bbad18a5873aeb3d07df25d9620b98d227206538
RyeCai <70669000+RyeCai@users.noreply.github.com>
2023-11-01 18:35:49 -0400

    initial commit

---
+++ src/pages/onboarding.tsx
@@ -0,0 +1,14 @@
+// Describe the solution you'd like as a user story
+// As a user, I want to enter information after registering to personalize my experience
+// As a user, I want to input dietary restrictions as a part of my profile
+// As a user, I want to input a name that the application will use
+
+// Definition of Done
+
+//     /onboarding implements the above
+//     User is redirected to the /onboarding page after registering
+
+// Additional context
+
+//     Use the AuthForm functional component as a skeleton
+//     Create a mutation procedure to update user information
