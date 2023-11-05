f8c9d95cd9c7d2231fbb42fe3a9aa9f9a4702d38
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-11-05 14:14:55 -0500

    feat: serving, time, and missing ingredient silders

--- src/components/search/RecipeFilters.tsx
+++ src/components/search/RecipeFilters.tsx
@@ -1,30 +1,68 @@
 import React from "react";
-import Checkbox from "~/components/search/Checkbox";
-
-function CheckboxGroup({
-  options,
-  label,
-}: {
-  label: string;
-  options: string[];
-}) {
-  return (
-    <>
-      <h3 className="mb-1 text-xl font-bold text-nobel-800">{label}</h3>
-      <div className="flex flex-col space-y-1">
-        {...options.map((option) => <Checkbox key={option} label={option} />)}
-      </div>
-    </>
-  );
-}
+import Slider from "~/components/search/Slider";
+import CheckboxGroup from "~/components/search/CheckboxGroup";
 
 export default function RecipeFilters() {
+  const [servings, setServings] = React.useState(2);
+  const [time, setTime] = React.useState(15);
+  const [missingIngredients, setMissingIngredients] = React.useState(0);
+
   return (
-    <div className="rounded-lg bg-nobel-300 p-2">
+    <form className="flex flex-col space-y-1 px-3 py-2">
       <CheckboxGroup
         label="Meal"
         options={["Breakfast", "Lunch", "Dinner", "Snack", "Drink"]}
       />
-    </div>
+      <CheckboxGroup
+        label="Restrictions"
+        options={["Keto", "Kosher", "Halal", "Vegetarian", "Vegan"]}
+      />
+      <CheckboxGroup
+        label="Allergens"
+        options={[
+          "Gluten Free",
+          "Dairy Free",
+          "Nut Free",
+          "Soy Free",
+          "Egg Free",
+          "Fish Free",
+        ]}
+      />
+      <Slider
+        label="Servings"
+        value={servings}
+        min={1}
+        max={6}
+        step={1}
+        onChange={setServings}
+      />
+      <Slider
+        label="Time"
+        hint="Maximum preparation time"
+        value={time}
+        min={15}
+        max={120}
+        transform={(v) => {
+          if (v < 60) return `< ${v}m`;
+
+          const hours = Math.floor(v / 60);
+          const minutes = v % 60;
+          const time = minutes > 0 ? `${hours}hr ${minutes}min` : `${hours}hr`;
+
+          return v === 120 ? `+${time}` : `< ${time}`;
+        }}
+        step={15}
+        onChange={setTime}
+      />
+      <Slider
+        label="Missing Ingredients"
+        hint={`Maxiumum number of ingredients missing from "My Pantry"`}
+        value={missingIngredients}
+        min={0}
+        max={5}
+        step={1}
+        onChange={setMissingIngredients}
+      />
+    </form>
   );
 }
---
+++ src/components/search/Slider.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import * as S from "@radix-ui/react-slider";
+import * as L from "@radix-ui/react-label";
+import * as T from "@radix-ui/react-tooltip";
+import FilterLabel from "./FilterLabel";
+
+interface Props {
+  label: string;
+  value: number;
+  min: number;
+  max: number;
+  step: number;
+  hint?: string;
+  onChange: (e: number) => void;
+  transform?: (e: number) => string;
+}
+
+export default function Slider({
+  hint,
+  label,
+  value,
+  min,
+  max,
+  step,
+  transform,
+  onChange,
+}: Props) {
+  const [showValue, setShowValue] = React.useState(false);
+
+  return (
+    <div>
+      <FilterLabel label={label} hint={hint} />
+      <div className="flex space-x-3">
+        <L.Root className="whitespace-nowrap text-sm text-nobel-600">
+          {transform ? transform(min) : min}
+        </L.Root>
+        <S.Root
+          className="relative col-span-2 flex h-5 w-full touch-none select-none items-center"
+          min={min}
+          max={max}
+          step={step}
+          value={[value]}
+          onValueChange={(e) => {
+            setShowValue(true);
+            onChange(e[0]!);
+          }}
+          onValueCommit={() => setShowValue(false)}
+          onBlur={() => setShowValue(false)}
+        >
+          <S.Track className="relative h-[3px] w-full rounded-full bg-white">
+            <S.Range className="absolute h-full rounded-full bg-white" />
+          </S.Track>
+          <S.Thumb
+            className="relative block h-4 w-4 rounded-[10px] bg-white shadow-[0_2px_4px] focus:shadow-[0_0_0_2px] focus:outline-none"
+            aria-label="Volume"
+          >
+            {showValue && (
+              <p className="absolute left-1/2 -translate-x-1/2 -translate-y-8 whitespace-nowrap rounded bg-white px-1 text-center font-bold text-nobel-500 shadow-[0_2px_4px]">
+                {transform ? transform(value) : value}
+              </p>
+            )}
+          </S.Thumb>
+        </S.Root>
+        <L.Root className="whitespace-nowrap text-sm text-nobel-600">
+          {transform ? transform(max) : max}
+        </L.Root>
+      </div>
+    </div>
+  );
+}
