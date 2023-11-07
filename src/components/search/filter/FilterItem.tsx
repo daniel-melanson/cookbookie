62c3cb15f0ad387c8eba45753d95cb98432bb74c
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-11-06 19:40:39 -0500

    feat: more accessable hint and filter add/remove

--- src/components/search/filter/FilterItem.tsx
+++ src/components/search/filter/FilterItem.tsx
@@ -1,43 +1,42 @@
 import React from "react";
-import * as Tooltip from "@radix-ui/react-tooltip";
 import * as Accordion from "@radix-ui/react-accordion";
-import { RiQuestionLine } from "react-icons/ri";
-
-function Hint({ text }: { text: string }) {
-  return (
-    <Tooltip.Provider>
-      <Tooltip.Root>
-        <Tooltip.Trigger disabled>
-          <RiQuestionLine className="mx-2 text-nobel-600 hover:cursor-pointer" />
-        </Tooltip.Trigger>
-        <Tooltip.Portal>
-          <Tooltip.Content
-            className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade mx-2 w-fit select-none rounded-lg bg-white px-3 py-2 text-center text-sm leading-none text-nobel-500 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
-            sideOffset={5}
-          >
-            {text}
-            <Tooltip.Arrow className="fill-white" />
-          </Tooltip.Content>
-        </Tooltip.Portal>
-      </Tooltip.Root>
-    </Tooltip.Provider>
-  );
-}
+import { RiAddLine, RiSubtractLine } from "react-icons/ri";
+import classNames from "classnames";
 
 export default function FilterItem({
   label,
   hint,
   children,
 }: React.PropsWithChildren<{ label: string; hint?: string }>) {
+  const baseClasses =
+    "absolute transform-y-1/2 transform-x-1/2 text-lg font-bold text-nobel-600";
+
   return (
     <Accordion.Item value={`accordion-item-${label.toLowerCase()}`}>
-      <Accordion.Header>
-        <Accordion.Trigger className="mb-1 flex items-center text-lg font-bold text-nobel-600 hover:cursor-pointer">
+      <Accordion.Header className="flex items-center">
+        <Accordion.Trigger className="group flex w-full items-center text-lg font-bold text-nobel-600 hover:cursor-pointer">
           {label}
-          {/* {hint && <Hint text={hint} />} */}
+          <div className="relative ml-auto h-4 w-4">
+            <RiAddLine
+              className={classNames(
+                baseClasses,
+                "invisible group-hover:visible group-data-[state=open]:invisible",
+              )}
+            />
+            <RiSubtractLine
+              className={classNames(
+                "invisible group-data-[state=open]:visible",
+              )}
+            />
+          </div>
         </Accordion.Trigger>
       </Accordion.Header>
-      <Accordion.Content className="overflow-hidden data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown">
+      <Accordion.Content className="overflow-hidden px-1 data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown">
+        {hint && (
+          <p className="disable-select mb-2 text-sm font-light italic text-nobel-500">
+            {hint}
+          </p>
+        )}
         {children}
       </Accordion.Content>
     </Accordion.Item>
