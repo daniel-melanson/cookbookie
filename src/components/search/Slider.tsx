4e31ad7068c97fc1f9b9fa8885ea4181b4384d08
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-11-14 13:11:58 -0500

    feat: value hints

--- src/components/search/Slider.tsx
+++ src/components/search/Slider.tsx
@@ -19,9 +19,31 @@ interface SingleSlider extends Props {
   onChange: (e: number) => void;
 }
 
-const SliderThumb = () => (
-  <S.Thumb className="relative block h-4 w-4 rounded-[10px] border-[1px] border-nobel-500 bg-white focus:border-none focus:shadow-[0_0_0_2px] focus:outline-none" />
-);
+function SliderThumb({
+  value,
+  onFocus,
+  shown,
+  onBlur,
+}: {
+  value: string;
+  shown?: boolean;
+  onFocus: () => void;
+  onBlur: () => void;
+}) {
+  return (
+    <S.Thumb
+      onFocus={onFocus}
+      onBlur={onBlur}
+      className="relative block h-4 w-4 rounded-[10px] border-[1px] border-nobel-500 bg-white focus:border-none focus:shadow-[0_0_0_2px] focus:outline-none"
+    >
+      {shown && (
+        <span className="absolute -top-8 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-sm bg-white p-0.5 text-sm text-nobel-500">
+          {value}
+        </span>
+      )}
+    </S.Thumb>
+  );
+}
 
 export default function Slider({
   value,
@@ -31,7 +53,10 @@ export default function Slider({
   transform,
   onChange,
 }: SingleSlider | RangeSlider) {
+  const valueToString = transform ?? ((e: number) => String(e));
+
   const [localValue, setLocalValue] = React.useState(value);
+  const [shownIndex, setShownIndex] = React.useState(-1);
 
   const isRange = typeof value !== "number";
   const [localLow, localHigh] = isRange
@@ -65,7 +90,7 @@ export default function Slider({
   return (
     <div className="flex space-x-3">
       <L.Root className="whitespace-nowrap text-sm text-nobel-600">
-        {transform ? transform(min) : min}
+        {valueToString(min)}
       </L.Root>
       <S.Root
         className="relative col-span-2 flex h-5 w-full touch-none select-none items-center"
@@ -85,11 +110,23 @@ export default function Slider({
             style={{ left: `${lowerPercent}%`, width: `${rangePercent}%` }}
           />
         </S.Track>
-        <SliderThumb />
-        {isRange && <SliderThumb />}
+        <SliderThumb
+          onFocus={() => setShownIndex(0)}
+          onBlur={() => shownIndex === 0 && setShownIndex(-1)}
+          shown={shownIndex === 0}
+          value={valueToString(localLow)}
+        />
+        {isRange && (
+          <SliderThumb
+            onFocus={() => setShownIndex(1)}
+            onBlur={() => shownIndex === 1 && setShownIndex(-1)}
+            shown={shownIndex === 1}
+            value={valueToString(localHigh)}
+          />
+        )}
       </S.Root>
       <L.Root className="whitespace-nowrap text-sm text-nobel-600">
-        {transform ? transform(max) : max}
+        {valueToString(max)}
       </L.Root>
     </div>
   );
