f30ee32b9d82469d2b6bbb0c4e5ca7e418381c30
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-11-15 09:32:51 -0500

    fix: don't remake schema each render

--- src/components/auth/SignUpPasswordField.tsx
+++ src/components/auth/SignUpPasswordField.tsx
@@ -1,18 +1,20 @@
-import { type ZodIssue } from "zod";
 import FormErrorMessage from "../form/FormErrorMessage";
 import FormTextField from "../form/FormTextField";
 import { useFormData } from "~/contexts/FormContext";
 import { password } from "~/utils/validators";
 
+const passwordSchema = password();
+
 export default function SignupPasswordField() {
   const data = useFormData();
-  const validatorResult = password().safeParse(data.password);
+  const validatorResult = passwordSchema.safeParse(data.password);
+
   return (
     <FormTextField name="password" type={"password"}>
       {!validatorResult.success &&
-        validatorResult.error.issues.map((e: ZodIssue) => {
-          return <FormErrorMessage key={e.message} message={e.message} />;
-        })}
+        validatorResult.error.issues.map((e) => (
+          <FormErrorMessage key={e.message} message={e.message} />
+        ))}
     </FormTextField>
   );
 }
