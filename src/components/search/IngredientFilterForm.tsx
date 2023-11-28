d5e409917fb8eb573393cd4efe2fa4f7090c2daf
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-11-28 15:09:55 -0500

    fix: ingredient filter form

--- src/components/search/IngredientFilterForm.tsx
+++ src/components/search/IngredientFilterForm.tsx
@@ -1,24 +1,22 @@
 import React from "react";
-import {
-  makeFormEventFactories,
-  useFilters,
-  useFiltersDispatch,
-} from "~/contexts/FilterContext";
 import FiltersLabel from "./FiltersLabel";
+import {
+  type IngredientFilters,
+  makeFormEventFactories,
+  useSearchArgs,
+  useSearchArgsDispatch,
+} from "~/contexts/SearchContext";
 
 export default function IngredientFilterForm() {
-  const filters = useFilters<IngredientFilters>();
-  const dispatch = useFiltersDispatch();
+  const args = useSearchArgs<IngredientFilters>();
+  const dispatch = useSearchArgsDispatch();
 
   const { onClearFactory, onChangeFactory, onAddFactory } =
-    makeFormEventFactories(filters, dispatch);
+    makeFormEventFactories(args, dispatch);
 
   return (
     <form className="w-72 space-y-1">
-      <FiltersLabel
-        showClear={Object.keys(filters).length > 0}
-        onClear={() => dispatch({ kind: "clear" })}
-      />
+      <FiltersLabel />
     </form>
   );
 }
